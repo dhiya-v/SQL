fe2ded74e6266e7798fee8762556854aae4849f1
Dhiya <dhiyavgeorgekutty@gmail.com>
2025-09-23 15:20:10 +0530

    added book table queries

---
+++ lab2.sql
@@ -0,0 +1,14 @@
+INSERT INTO books (id, title, author, price, stock) VALUES (1, 'The Alchemist', 'Paulo Coelho', 350, 50),
+(2, 'Atomic Habits', 'James Clear', 450, 40),
+(3, 'The Psychology of Money', 'Morgan Housel', 400, 30),
+(4, 'Ikigai', 'Francesc Miralles', 300, 60),
+(5, 'Deep Work', 'Cal Newport', 500, 20);
+
+SELECT * FROM books WHERE price<450 AND stock>30; 
+
+UPDATE books SET stock=45 , price=420 where title ='Deep Work';
+
+DELETE FROM books WHERE title='Ikigai';
+
+SELECT AVG(price) AS AVG_PRICE, COUNT(*) AS TOTAL FROM books; 
+
