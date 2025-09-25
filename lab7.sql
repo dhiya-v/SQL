a715801836ab64e0cb4db1d29e8f36b8abbf3270
Dhiya <dhiyavgeorgekutty@gmail.com>
2025-09-25 10:58:52 +0530

    added groceryshop db and tables

---
+++ lab7.sql
@@ -0,0 +1,5 @@
+CREATE TABLE products (product_id INT AUTO_INCREMENT PRIMARY KEY ,product_name VARCHAR(100) ,price INT);
+
+ALTER TABLE products ADD COLUMN category VARCHAR(100);
+
+TRUNCATE TABLE products;
