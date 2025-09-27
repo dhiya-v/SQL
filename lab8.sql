341a556dbf0f0f05c49d7957933df8ca849e5199
Dhiya <dhiyavgeorgekutty@gmail.com>
2025-09-27 22:06:19 +0530

    added database BookStoreDB and tables-authors and books

---
+++ lab8.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE BookStoreDB;
+CREATE TABLE authors ( author_id INT PRIMARY KEY, name VARCHAR(100), country VARCHAR(100) ); 
+CREATE TABLE books ( book_id INT PRIMARY KEY, title VARCHAR(100), price INT, author_id INT, FOREIGN KEY (author_id) REFERENCES authors (author_id) ); 
+ALTER TABLE books ADD COLUMN published_year INT ;
+TRUNCATE TABLE books;
